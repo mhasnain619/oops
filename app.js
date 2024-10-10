1bdff62c2a13b3fc8d17645da03c43464cc9207d
Muhammad Hasnain <hasnain0893@gmail.com>
2024-10-10 11:57:02 +0500

    ds

--- app.js
+++ app.js
@@ -11,6 +11,18 @@ class User {
     }
 }
 
+class Admin extends User {
+    constructor(name, email) {
+        super(name, email);
+    }
+    editData() {
+        data = 'Some new DAta'
+    }
+}
+
+
 let student1 = new User('AnAINTAY', 'anain@gmail.com')
 let student2 = new User('anain', 'anain1@gmail.com')
 let student3 = new User('ana', 'anain2@gmail.com')
+
+let admin1 = new User('admin1', 'admin@gmail.com')
