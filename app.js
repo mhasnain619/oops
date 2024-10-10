903effee042286a09946441ffdfc9d7fc3d55c94
Muhammad Hasnain <hasnain0893@gmail.com>
2024-10-11 00:33:46 +0500

    sf

--- app.js
+++ app.js
@@ -35,9 +35,83 @@
 
 
 
-for (let i = 0; i < 5; i++) {
-    let str = ''
-    for (let j = 0; j < 5; j++)
-        str = str + j
-    console.log(i, str)
+// for (let i = 0; i < 5; i++) {
+//     let str = ''
+//     for (let j = 0; j < 5; j++)
+//         str = str + j
+//     console.log(i, str)
+// }
+
+
+// function getData(dataid, getNextData) {
+//     setTimeout(() => {
+//         console.log('data', dataid);
+//         if (getNextData) {
+//             getNextData()
+//         }
+
+//     }, 2000);
+// }
+
+
+//callback hell
+
+// getData(1, () => {
+//     console.log('getting data 2');
+
+//     getData(2, () => {
+//         console.log('getting data 3');
+
+//         getData(3, () => {
+//             console.log('getting data 4');
+
+//             getData(4)
+//         })
+//     })
+// })
+
+
+// promises
+
+
+// let promise = new Promise((resolve, reject) => {
+//     console.log('i am promise');
+//     reject('erroer  occuring')
+// })
+
+// function getData(dataid, getNextData) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log('data', dataid);
+//             resolve('Sucess')
+//             if (getNextData) {
+//                 getNextData()
+//             }
+
+//         }, 5000);
+//     }
+//     )
+// }
+
+
+// const getPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         console.log('i am promise');
+//         reject('Success')
+//     })
+// }
+
+// let promise = getPromise()
+// promise.then((data) => {
+//     console.log('promise full filled', data);
+// })
+//     .catch((error) => {
+//         console.log(' error occur', error);
+
+//     })
+
+
+async function hello() {
+    console.log('hello');
+
 }
